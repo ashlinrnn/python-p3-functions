a5b7add775d595fdc8b6d8de040bff0df61fff7c
Ashlin Rofina <ashlinrofina@gmail.com>
2025-12-11 11:22:23 +0300

    adds codes

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(): 
+    print("Hello, programmer!")
+   
 
-def greet(name):
-    pass
+def greet(name="Guido"): 
+    print(f"Hello, {name}!")
+   
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!") 
+    
 
-def add(num1, num2):
-    pass
+    
+    
+def add(num1, num2): 
+    return num1 + num2
+  
 
-def halve(number):
-    pass
+def halve(number): 
+    return number / 2
+   
